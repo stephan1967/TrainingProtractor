201719054e3a772730542e9a8122a7ff66922c19
Stephan Scherders <35383746+stephan1967@users.noreply.github.com>
2019-05-04 13:26:48 +0200

    Update javascriptBasis.spec.js

--- tests/javascriptBasis/javascriptBasis.spec.js
+++ tests/javascriptBasis/javascriptBasis.spec.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+var javascriptBasisPo = require('./javascriptBasis.po');
+var javascriptBasisTestData = require('./javascriptBasis.testData');
+
+describe('Learn basic Javascript ', function() {
+    
+    it('1. Call a function', function() {
+        javascriptBasisPo.exampleFunction();
+    });
+
+    it('2. Use an array', function() {
+
+    });
+
+    it('3. Use a for-loop', function() {
+
+    });
+});
