5b5e4e0b44a132b088a01e6f1496d92f29ba32fb
Stephan Scherders <35383746+stephan1967@users.noreply.github.com>
2019-05-04 13:28:06 +0200

    Create javascriptBasis.testData.js

---
+++ tests/javascriptBasis/javascriptBasis.testData.js
@@ -0,0 +1,10 @@
+var JavascriptBasisTestData = {
+    name1: 'Stephan',
+	name2: 'Ilyass',
+    name3: 'Lixiong',
+    name4: 'Mieke',
+    name5: 'Arthur',
+    name6: 'Harald',
+};
+
+module.exports = JavascriptBasisTestData;
