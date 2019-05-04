d4e9d0c5c08407752af0ab290b07210d8fa834fe
Stephan <Stephan@LAPTOP-STEPHAN>
2019-05-04 22:45:09 +0200

    Welkomst message aangepast.

--- tests/javascriptBasis/javascriptBasis.po.js
+++ tests/javascriptBasis/javascriptBasis.po.js
@@ -1,17 +1,17 @@
 var JavascriptBasis = {
 
     exampleFunction: function() {
-    	console.log('Mijn naam is Stephan');
+    	console.log('Mijn naam is Stephan Scherders uit Utrecht.');
     },
 
     exampleArray: function() {
- 
+
 
     	// More info about array: https://www.w3schools.com/js/js_arrays.asp
     },
 
     exampleForLoop: function(array) {
-    	
+
 
     	// More info about for loop: https://www.w3schools.com/js/js_loop_for.asp
     }
--- tests/javascriptBasis/javascriptBasis.testData.js
+++ tests/javascriptBasis/javascriptBasis.testData.js
@@ -1,6 +1,6 @@
 var JavascriptBasisTestData = {
     name1: 'Stephan',
-	name2: 'Ilyass',
+	  name2: 'Ilyass',
     name3: 'Lixiong',
     name4: 'Mieke',
     name5: 'Arthur',
