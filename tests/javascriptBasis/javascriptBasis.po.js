c3eee984b918fa2ce8dadc0a46bdc5d9b7284e9a
Stephan Scherders <35383746+stephan1967@users.noreply.github.com>
2019-05-04 13:27:35 +0200

    Create javascriptBasis.po.js

---
+++ tests/javascriptBasis/javascriptBasis.po.js
@@ -0,0 +1,20 @@
+var JavascriptBasis = {
+
+    exampleFunction: function() {
+    	console.log('Mijn naam is Stephan');
+    },
+
+    exampleArray: function() {
+ 
+
+    	// More info about array: https://www.w3schools.com/js/js_arrays.asp
+    },
+
+    exampleForLoop: function(array) {
+    	
+
+    	// More info about for loop: https://www.w3schools.com/js/js_loop_for.asp
+    }
+};
+
+module.exports = JavascriptBasis;
